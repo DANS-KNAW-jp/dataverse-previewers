f66ed0fb216195b68e17a97349a06b14880d9af9
jo-pol <jo-pol@users.noreply.github.com>
2024-11-18 18:15:18 +0100

    find subtitle files in datasetMetadata

--- previewers/v1.4.2/js/video.js
+++ previewers/v1.4.2/js/video.js
@@ -8,11 +8,39 @@ function translateBaseHtmlPage() {
 }
 
 function writeContent(fileUrl, file, title, authors) {
+
+    queryParams = new URLSearchParams(window.location.search.substring(1));
+    var versionUrl = queryParams.get("siteUrl") + "/api/datasets/"
+        + queryParams.get("datasetid") + "/versions/"
+        + queryParams.get("datasetversion");
+
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        crosssite: true,
+        url: versionUrl,
+        success: function(data, status) {
+            writeContentAndFiles(fileUrl, file, title, authors, data.data.files, queryParams.get("siteUrl"));
+        },
+        error: function(request, status, error) {
+            reportFailure(
+                "Unable to retrieve file.",
+                status);
+        }
+    });
+}
+
+function writeContentAndFiles(fileUrl, file, title, authors, files, siteUrl) {
     addStandardPreviewHeader(file, title, authors);
-    const subtitles = new Map([
-        ["de", "https://raw.githubusercontent.com/DANS-KNAW-jp/html5-video-webvtt-example/refs/heads/master/MIB2-subtitles-de-BR.vtt"],
-        ["pt", "https://raw.githubusercontent.com/DANS-KNAW-jp/html5-video-webvtt-example/refs/heads/master/MIB2-subtitles-pt-BR.vtt"],
-    ]);
+
+    const regex = /\.([a-z]+)\.vtt$/i
+    const subtitles = files
+        .filter(item => regex.test(item.label))
+        .reduce((map, item) => {
+            const lang = item.label.match(regex)[1];
+            map.set(lang, `${siteUrl}/api/access/datafile/${item.dataFile.id}?gbrecs=true&amp;key=93423e09-848c-47cb-a979-219dafcfa4da`);
+            return map;
+    }, new Map());
 
     const videoElement = $("<video/>").prop("controls", true)
         .append($('<source/>').attr("src", fileUrl));
